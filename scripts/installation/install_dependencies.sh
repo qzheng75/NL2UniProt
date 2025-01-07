ae7c7b65c0ffceb0b84275e4ae666b57cce29968
Qianyu Zheng <113787170+qzheng75@users.noreply.github.com>
2025-01-07 10:55:49 -0500

    Update install_dependencies.sh

--- scripts/installation/install_dependencies.sh
+++ scripts/installation/install_dependencies.sh
@@ -28,8 +28,8 @@ pre-commit install
 clear
 echo "Installation complete! Creating .env file..."
 touch .env
-echo "export MODEL_CACHE=model_cache" >> .env
-echo "export GOOGLE_APPLICATION_CREDENTIALS=google_cloud/credential.json" >> .env
+echo "MODEL_CACHE=model_cache" >> .env
+echo "GOOGLE_APPLICATION_CREDENTIALS=google_cloud/credential.json" >> .env
 echo "Complete! You can adjust the .env file as needed."
 
 echo "===================="
